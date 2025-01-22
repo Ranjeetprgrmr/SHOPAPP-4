62e4080da3298427d82743eae1bd11d30c9cd864
Ranjeet Singh Tharu <67905362+Ranjeetprgrmr@users.noreply.github.com>
2025-01-22 21:54:15 +0545

    complete product controller

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,34 +1,12 @@
 import express from "express";
-import Product from "../models/productModel.js";
-import asyncHandler from "express-async-handler";
+import { getProductsById, getProducts } from "../controllers/productController.js";
 
 const router = express.Router();
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    // console.log('this is products',products)
-    if (products) {
-      res.json(products);
-    }
-  })
-);
+// router.get("/", getProducts);
 
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    // console.log('this is req', req);
-    // console.log("this is id", req.params);
-
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found");
-    }
-  })
-);
+// router.get("/:id", getPoductById);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductsById);
 
 export default router;
